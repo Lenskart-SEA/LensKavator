6fb5250ac99eece27c94f6e8bd42bd9852a160b8
Lenskart-SEA <99468990+Lenskart-SEA@users.noreply.github.com>
2022-02-11 15:53:16 +0800

    Boiler plate

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const db = require('./model/db');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', express.static('public'))
+
+app.get('/api/excavators', (req, res) => {
+    let data = db.read('db.json');
+    res.send(data);
+})
+
+app.get("*", (req, res) => {
+    res.sendStatus(404)
+})
+
+app.listen(8080, ()=> "Listening on Port 8080")
